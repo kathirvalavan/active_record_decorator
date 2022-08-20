923d2dfc975498c3d344c0631ff307fbde1a1ff6
kathir <kathirvalan.ict@gmail.com>
2022-08-20 23:36:46 +0530

    fixes and corrections

--- lib/active_record_decorator.rb
+++ lib/active_record_decorator.rb
@@ -2,6 +2,27 @@
 module ActiveRecordDecorator
   extend ActiveSupport::Concern
 
+  class Callback
+
+    attr_reader :relation_name, :method_name, :relation_class
+
+    def initialize(relation_type, relation_name, relation_class, method_name)
+      @relation_type = relation_type
+      @relation_name = relation_name
+      @method_name   = method_name
+      @relation_class = relation_class
+    end
+
+    def build(relation_name, method_name)
+      new(relation_name, method_name)
+    end
+
+    def relation_class_match?(rl_class)
+      rl_class == @relation_class
+    end
+
+  end
+
   included do
     class_attribute :relation_callback_chain
     self.relation_callback_chain = Concurrent::Array.new
