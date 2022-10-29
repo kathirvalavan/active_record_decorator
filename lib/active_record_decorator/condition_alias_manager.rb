2c5e615685ecc8fb9137c1d0225bee4f0f0ed254
kathir <kathirvalan.ict@gmail.com>
2022-10-30 00:56:22 +0530

    operators added

--- lib/active_record_decorator/condition_alias_manager.rb
+++ lib/active_record_decorator/condition_alias_manager.rb
@@ -5,6 +5,12 @@ module ActiveRecordDecorator
       case operator_name.to_sym
       when :equal
         ActiveRecordDecorator::ConditionEvaluatorCore::Operators::Equal.new
+      when :not_equal
+        ActiveRecordDecorator::ConditionEvaluatorCore::Operators::NotEqual.new
+      when :greater_than
+        ActiveRecordDecorator::ConditionEvaluatorCore::Operators::GreaterThan.new
+      when :lesser_than
+        ActiveRecordDecorator::ConditionEvaluatorCore::Operators::LesserThan.new
       else
         raise 'Not implemented'
       end
--- lib/active_record_decorator/condition_evaluator_core/operators.rb
+++ lib/active_record_decorator/condition_evaluator_core/operators.rb
@@ -15,6 +15,24 @@ module ActiveRecordDecorator
         end
       end
 
+      class NotEqual < Operator
+        def evaluate(value_1, value_2)
+          value_1.value != value_2.value
+        end
+      end
+
+      class GreaterThan < Operator
+        def evaluate(value_1, value_2)
+          value_1.value > value_2.value
+        end
+      end
+
+      class LesserThan < Operator
+        def evaluate(value_1, value_2)
+          value_1.value < value_2.value
+        end
+      end
+
     end
   end
 end
